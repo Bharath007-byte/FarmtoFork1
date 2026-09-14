147b61140c2edff4fccf2b03a7ae231b2fcaea00
Bharath007-byte <chatraju.bharath@s.amity.edu>
2026-09-14 12:06:30 +0530

    fix(cache): send no-cache headers to guarantee fresh bundle loading on client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1012,6 +1012,9 @@ for (const distPath of candidateDistPaths) {
       ) {
         return next();
       }
+      res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+      res.setHeader("Pragma", "no-cache");
+      res.setHeader("Expires", "0");
       res.sendFile(path.join(distPath, "index.html"));
     });
     break;
